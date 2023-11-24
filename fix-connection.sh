a5ed6d7c2657389ade893dfab4078f218c819e03
Ali Kadir Bagcioglu <alikadirbagcioglu@gmail.com>
2023-11-24 19:44:09 +0300

    Update fix-connection.sh

--- fix-connection.sh
+++ fix-connection.sh
@@ -1,9 +1,9 @@
-echo "system checking..."
-sleep 300
+echo "system checking... AKB"
+sleep 300 
 sudo systemctl status homebridge.service | grep dead
 if [[ $? == 0 ]]; then
   echo "system restart by AKB" 
   sudo reboot
 else
-  echo "system runing"
+  echo "system runing AKB"
 fi
